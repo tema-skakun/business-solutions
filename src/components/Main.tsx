1204d5f7bf430639891f4ea1132bc05f42ed6a31
tema-skakun <frederico_f@mail.ru>
2024-07-19 15:43:17 +0300

    refactor: adds saving page number

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { formatName } from '../utils/formatName';
 import Pagination from './Pagination';
 import './Main.css';
@@ -16,7 +16,15 @@ interface MainProps {
 const REVIEWS_PER_PAGE = 10;
 
 const Main: React.FC<MainProps> = ({ reviews }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState<number>(() => {
+    const savedPage = localStorage.getItem('currentPage');
+    return savedPage ? Number(savedPage) : 1;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('currentPage', currentPage.toString());
+  }, [currentPage]);
+
   const reviewKeys = Object.keys(reviews);
   const totalReviews = reviewKeys.length;
 
@@ -24,10 +32,7 @@ const Main: React.FC<MainProps> = ({ reviews }) => {
   const indexOfFirstReview = indexOfLastReview - REVIEWS_PER_PAGE;
   const currentReviews = reviewKeys.slice(indexOfFirstReview, indexOfLastReview);
 
-  const paginate = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <div className="main">
