ba1be59c6ad04094bf8e29a60c7af2f0aca58e1f
tema-skakun <frederico_f@mail.ru>
2024-07-21 00:58:35 +0300

    fix: corrects warning in useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 function useLocalStorage<T>(key: string, initialValue: T) {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      if (item) {
+        // проверка корректности JSON
+        if (/^[\],:{}\s]*$/.test(item
+          .replace(/\\["\\bfnrtu]/g, '@')
+          .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?/g, ']')
+          .replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+          return JSON.parse(item);
+        } else {
+          console.warn(`Invalid JSON data in localStorage for key "${key}". Resetting to initial value.`);
+          localStorage.removeItem(key);  // очистка некорректных данных
+          return initialValue;
+        }
+      } else {
+        return initialValue;
+      }
     } catch (error) {
-      console.error(error);
+      console.error(`Error reading localStorage key "${key}":`, error);
       return initialValue;
     }
   });
@@ -16,7 +30,7 @@ function useLocalStorage<T>(key: string, initialValue: T) {
       setStoredValue(value);
       localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
-      console.error(error);
+      console.error(`Error setting localStorage key "${key}":`, error);
     }
   };
 
